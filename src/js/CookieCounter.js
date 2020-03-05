c38670adde0c3f4900e2e654a441f88a948bf3ac
Aaron <southpawflash@hotmail.com>
2020-03-05 09:46:24 -0500

    have buttons working, but not getting numbers to show or count

---
+++ src/js/CookieCounter.js
@@ -0,0 +1,53 @@
+// const clicks = 0;
+// const clickPS =0;
+// const clicksPC = 1;
+
+// function gainClick() {
+//   clicks += clicksPC;
+// }
+
+// setInterval(function showClicksPS() {
+
+//   document.getElementById("clicks").innerHTML = "Clicks: " + clicks;
+
+// })
+// setInterval(function showClicksPS() {
+//   document.getElementById("clicksPS").innerHTML = "Clicks per second: " + clickPS;
+  
+// })
+// function getClicksPS(){
+//   if (clicks >= 15){
+//     clicksPS +=1;
+//     clicks -= 15;
+//   }
+//   else{
+//     alert("Sorry, you need to click more")
+//   }
+//   if (addInterval) window.clearTimeout(addInterval)
+//     addInterval = setInterval(function clicksPS(){
+//       clicks += clicksPS
+//     }, 100)
+//     }
+
+  // this.registerClick = function() {
+  //     this.clicksPS(this.numberOfClicks() + 1);
+  // };
+
+//   this.resetClicks = function() {
+//       this.numberOfClicks(0);
+//   };
+
+//   this.hasClickedTooManyTimes = ko.pureComputed(function() {
+//       return this.numberOfClicks() >= 3;
+//   }, this);
+// };
+
+// ko.applyBindings(new ClickCounterViewModel());
+
+const gameClicker ={
+  click: 0,
+  clickPC: 1
+}
+function countClick(){
+  gameClicker.click += gameClicker.clickPC
+}
