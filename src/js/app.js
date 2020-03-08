35376eb1092dc6a9bde49663607d6bd1ee3399e0
Aaron <southpawflash@hotmail.com>
2020-03-08 17:17:04 -0400

    functionality of click game with multi/reset/buy - works, drop downs not working

--- src/js/app.js
+++ src/js/app.js
@@ -66,46 +66,65 @@ let gen1amount = 0,
     credit = 0, 
     cps = 0;
     creditbuy = 0;
+    extraClicks = 150;
 
     function update(){
         click.onclick = function() {credit = credit + 1};
         generator1.onclick = function() {
-        if (credit >= gen1price) {
-            credit = credit - gen1price;
-            gen1amount = gen1amount + 1;
-            cps = cps + 1;
-            gen1price = gen1baseprice * (Math.pow(1.2, gen1amount));
+            if (credit >= gen1price) {
+                credit = credit - gen1price;
+                gen1amount = gen1amount + 1;
+                cps = cps + 1;
+                gen1price = gen1baseprice * (Math.pow(1.2, gen1amount));
             }
             else{
                 alert("You need more clicks")
             }
-            };                        
+        }                        
         
         credit = credit + (cps/60);
-    };
+    }
 
-        function draw(){
+    function draw(){
         creditPerSecond.value = cps.toFixed(0);
         creds.value = credit.toFixed(0);
         generator1.value = gen1price.toFixed(0);
         generator1amount.value = gen1amount.toFixed(0);
-    };
-    function buyclicks(){
-        if(credit>= ((creditbuy +1) * 150)){
-            credit = credit - ((creditbuy +1) * 150);
-            creditbuy = creditbuy + 1;
-            update()
-
-        }
-        
     }
-     let mainloop = function() {
-        update(), buyclicks(), draw()}; 
+
+        
+    
+    let mainloop = function() {
+        update(), draw(), reset(), buyClick()
+    }; 
     setInterval(mainloop, 16);
+     
     
     function reset(){
-        click.onclick = function(){ update() * 0 };
+        resetClick.onclick = function(){ 
+            cps = 0,
+            gen1amount = 0,
+            gen1price = 10;
+            gen1baseprice = 100;
+            credit = 0, 
+            creditbuy = 0; 
+        };
     }
+    function buyClick(){
+        buyClicks.onclick = function(){
+            if(credit >= 150){
+                credit += 50;  
+            }
+        };
+    }
+
+    function toggleClass(){
+        let menu = document.querySelector(".mainMenu");
+        menu.classList.toggle("toggleCls");
+    }
+    let hambuger = document.querySelector(".hamIcon");
+    
+    hambuger.addEventListener("click", toggleClass);
  
 
     
